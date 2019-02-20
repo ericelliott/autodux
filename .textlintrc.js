d93f3a80478f258a952b216180fd9d92be451716
Konstantin Grushetsky <github@grushetsky.net>
2019-02-20 18:31:59 +0300

    Add lint for Markdown files using 'markdownlint' and 'textlint'
    Add checking for common misspellings, wrong terminology and usage of invalid characters.

---
+++ .textlintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  filters: {},
+  rules: {
+    '@textlint-rule/no-invalid-control-character': true,
+    'common-misspellings': {
+      ignore: []
+    },
+    terminology: {
+      defaultTerms: true
+    }
+  }
+};

aa3085f6f7229e75d32f5cadae37b22e7b82e4b5
Konstantin Grushetsky <github@grushetsky.net>
2019-02-20 18:31:59 +0300

    Add lint for Markdown files using 'markdownlint' and 'textlint'
    Add checking for common misspellings, wrong terminology and usage of invalid characters.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   linters: {
     '*.js': ['eslint --fix', 'git add'],
+    '*.md': ['prettier --write', 'markdownlint', 'textlint', 'git add'],
     '*.{json,yml}': ['prettier --write', 'git add']
   }
 };

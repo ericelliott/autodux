c638bbc109e986c6846a524a56dec72c241719bc
Konstantin Grushetsky <github@grushetsky.net>
2019-02-25 02:38:32 +0300

    Refactor creating default actions creators, reducers and selectors
    Separate processing of default and custom entities.
    Extract function for creating action creator.
    Extract function for creating reducer.
    Rename function for creating selector.

--- src/core.js
+++ src/core.js
@@ -12,90 +12,92 @@ import {
   getType
 } from './helpers';
 
-// # Selector creation:
-const sliceSelector = (slice, fn) => state => fn(state[slice], state);
-
-const createDefaultSelectors = (slice, initial) =>
-  Object.keys(initial).reduce(
-    (obj, key) =>
-      slice
-        ? Object.assign(obj, {
-            [getSelectorName(key)]: sliceSelector(slice, state =>
-              prop(key, state)
-            )
-          })
-        : Object.assign(obj, {
-            [getSelectorName(key)]: state => prop(key, state)
-          }),
-    {}
-  );
-
-const createSliceSelectors = (slice, selectors) =>
-  Object.assign(
-    Object.keys(selectors).reduce(
-      (obj, key) =>
-        slice
-          ? Object.assign(obj, {
-              [key]: sliceSelector(slice, selectors[key])
-            })
-          : Object.assign(obj, { [key]: selectors[key] }),
-      {}
-    ),
-    {
-      [getSelectorName(slice)]: sliceSelector(slice, id)
-    }
-  );
-// /selector creation
-
-// # Action creation
-const createAction = (slice, action, key, type = getType(slice, action)) => ({
-  create: Object.assign(payload => payload, { type }),
-
-  reducer: (state, payload) =>
-    !key && isPrimitive(payload)
-      ? payload
-      : Object.assign({}, state, key ? { [key]: payload } : payload)
-});
-
-const getInitialActions = (slice, initial) =>
-  Object.keys(initial).reduce((o, key) => {
-    const action = getActionCreatorName(key);
-    return Object.assign(o, {
-      [action]: createAction(slice, action, key)
-    });
-  }, {});
-
-const addDefaultActions = (slice, initial, actions) => {
-  const initialActions = getInitialActions(slice, initial);
-  const action = getActionCreatorName(slice);
-
+const createActionCreator = (slice, name, mapPayload) => {
+  const type = getType(slice, name);
   return Object.assign(
-    {},
-    {
-      [action]: createAction(slice, action)
-    },
-    initialActions,
-    actions
+    (...args) => ({
+      type,
+      payload: isFunction(mapPayload) ? mapPayload(...args) : args[0]
+    }),
+    { type }
   );
 };
 
-const createMappedActions = (slice, actions) =>
+const createReducer = key => (state, payload) =>
+  !key && isPrimitive(payload)
+    ? payload
+    : Object.assign({}, state, key ? { [key]: payload } : payload);
+
+const createSelector = (slice, fn) => state => fn(state[slice], state);
+
+/**
+ * Create default action creators, reducers and selectors.
+ * @param {string} slice - Slice name
+ * @param {object} initial - Initial state object
+ * @returns {[object, object, object]} Tuple containing action creator, reducer and selector.
+ */
+const createDefaults = (slice, initial) => {
+  const sliceActionCreatorName = getActionCreatorName(slice);
+
+  return Object.keys(initial).reduce(
+    (resultTuple, key) => {
+      const actionCreatorName = getActionCreatorName(key);
+
+      return [
+        Object.assign(resultTuple[0], {
+          [actionCreatorName]: createActionCreator(slice, actionCreatorName)
+        }),
+        Object.assign(resultTuple[1], {
+          [actionCreatorName]: createReducer(key)
+        }),
+        Object.assign(resultTuple[2], {
+          [getSelectorName(key)]: createSelector(
+            slice,
+            state => prop(key, state)
+          )
+        })
+      ];
+    },
+    [
+      {
+        [sliceActionCreatorName]: createActionCreator(
+          slice,
+          sliceActionCreatorName
+        )
+      },
+      {
+        [sliceActionCreatorName]: createReducer()
+      },
+      {
+        [getSelectorName(slice)]: createSelector(
+          slice,
+          id
+        )
+      }
+    ]
+  );
+};
+
+const createActionCreators = (slice, actions) =>
   Object.keys(actions).reduce(
     (obj, action) =>
       Object.assign({}, obj, {
-        [action]: Object.assign(
-          (...args) => ({
-            type: getType(slice, action),
-            payload: isFunction(actions[action].create)
-              ? actions[action].create(...args)
-              : args[0]
-          }),
-          { type: getType(slice, action) }
+        [action]: createActionCreator(slice, action, actions[action].create)
+      }),
+    {}
+  );
+
+const createSelectors = (slice, selectors) =>
+  Object.keys(selectors).reduce(
+    (obj, key) =>
+      Object.assign(obj, {
+        [key]: createSelector(
+          slice,
+          selectors[key]
         )
       }),
     {}
   );
-// /action creation
 
 const checkOptions = ({ slice }) => {
   if (!isSliceValid(slice)) {
@@ -107,45 +109,51 @@ export default function autodux(options = {}) {
   checkOptions(options);
 
   const { initial = '', actions = {}, selectors = {}, slice } = options;
+
+  const [
+    defaultActionCreators,
+    defaultReducers,
+    defaultSelectors
+  ] = createDefaults(slice, initial);
+
   const allSelectors = Object.assign(
     {},
-    createDefaultSelectors(slice, initial),
-    createSliceSelectors(slice, selectors)
+    defaultSelectors,
+    createSelectors(slice, selectors)
   );
 
-  const allActions = createMappedActions(
-    slice,
-    addDefaultActions(slice, initial, actions)
+  const allActions = Object.assign(
+    {},
+    defaultActionCreators,
+    createActionCreators(slice, actions)
   );
 
-  const reducer = (state = initial, { type, payload } = {}) => {
+  const rootReducer = (state = initial, { type, payload } = {}) => {
     const [namespace, subType] = type
       ? type.split('/')
       : getType('unknown', 'unknown').split('/');
-
-    const defaultActions = addDefaultActions(slice, initial, {});
 
     // Look for reducer with top-to-bottom precedence.
     // Fall back to default actions, then undefined.
     // The actions[subType] key can be a function
     // or an object, so we only select the value
     // if it's a function:
-    const actionReducer = [
+    const reducer = [
       path([subType, 'reducer'], actions),
       actions[subType],
-      path([subType, 'reducer'], defaultActions)
+      defaultReducers[subType]
     ].reduceRight((f, v) => selectIfFunction(v) || f);
 
-    return namespace === slice && (actions[subType] || defaultActions[subType])
-      ? actionReducer
-        ? actionReducer(state, payload)
+    return namespace === slice && (actions[subType] || defaultReducers[subType])
+      ? reducer
+        ? reducer(state, payload)
         : Object.assign({}, state, payload)
       : state;
   };
 
   return {
     initial,
-    reducer,
+    reducer: rootReducer,
     slice,
     selectors: allSelectors,
     actions: allActions

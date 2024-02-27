d5f6ddc912980c7566a61d53afe0c71133c1f2a7
Eric Elliott <support@paralleldrive.com>
2024-02-26 18:03:54 -0800

    Fix lint-staged config

--- lint-staged.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  linters: {
-    '*.js': ['eslint --fix', 'git add'],
-    '*.md': ['markdownlint', 'textlint', 'git add'],
-    '*.{json,yml}': ['prettier --write', 'git add']
-  }
-};
